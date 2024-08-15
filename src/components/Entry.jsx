27056de02e39cf3d5fe27bedec9aea588af58ac3
Harsh Vardhan <harshvardhan1412002@gmail.com>
2024-08-15 22:25:20 +0530

    changes

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,20 +1,4 @@
-/*import React from "react";
-function Entry(props){
-    return(
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              {props.emoji}
-            </span>
-            <span>{props.name}</span>
-          </dt>
-          <dd>
-                {props.description}
-          </dd>
-        </div>
-    );
-}
-export default Entry;*/
+
 import React from "react";
 
 function Entry(props) {
